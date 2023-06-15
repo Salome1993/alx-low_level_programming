bb0f2a915d6ff54f3893d64b03ed0ee0c69276c0
Salome <sally.ndadza@gmail.com>
2023-06-15 06:19:53 -0700

    correct

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - A Program that print a line ,floowed by a new line to the standard error
- * Return: Always 0 (success)
+ * main - Entry
+ * Return: 1
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"));
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
