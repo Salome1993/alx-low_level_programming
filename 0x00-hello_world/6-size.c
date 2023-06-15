01fec72d1fe515b06e5e43afed6217a397a10e46
Salome <sally.ndadza@gmail.com>
2023-06-15 04:34:35 -0700

    correct

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - A program that prints the size o various types on the computer
+ * Return: 0
+ */
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float f;
+
+	printf("size of a char:
+
