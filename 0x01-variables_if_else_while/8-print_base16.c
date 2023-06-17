968c62f40e5caaed4cf6ffc395341ee256ef8b78
Salome <sally.ndadza@gmail.com>
2023-06-17 08:16:13 -0700

    tSK8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ * Return: 0 Always (Success)
+ */
+
+int main(void)
+{
+	char low;
+	int d;
+
+	for (d = '0'; d <= '9'; d++)
+		putchar(d);
+	for (low = 'a'; low <= 'f'; low++)
+		putchar(low);
+	putchar('\n');
+	return (0);
+}
